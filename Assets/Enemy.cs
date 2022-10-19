202a38a36151e82a813754655846259f89d36e70
Finn Pauls <mbp@pauls.dev>
2022-10-19 14:56:32 +0200

    Game Over when enemy touches Player

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -62,4 +62,11 @@ public class Enemy : MonoBehaviour
 
     public void OnDetectionLost() {
     }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Destroy(other.gameObject);
+            Game.GameOver();
+        }
+    }
 }
--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
-    public static bool isDark = false;
+    public static bool isDark = true;
 
     public Light gameLight;
 
@@ -19,12 +20,26 @@ public class Game : MonoBehaviour
         }
     }
 
+    void Start() {
+        SetDarkness(true);
+    }
+
     public static void ToggleDarkness() {
-        isDark = !isDark;
+        SetDarkness(!isDark);
+    }
+
+    public static void SetDarkness(bool isDark) {
+        Game.isDark = isDark;
         if (isDark) {
             Instance.gameLight.intensity = 0.0f;
         } else {
             Instance.gameLight.intensity = 0.4f;
         }
+    }
+
+    public static void GameOver() {
+        SetDarkness(true);
+        
+        SceneManager.LoadScene("GameOver");
     }
 }
